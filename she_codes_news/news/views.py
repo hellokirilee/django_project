4ad0692c5dd281aa26049f79b93174d9f8a87170
hellokirilee <hello.kiri.lee@gmail.com>
2020-08-14 18:05:10 +0800

    updated fixtures, moving to pc

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -21,7 +21,8 @@ class IndexView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['latest_stories'] = NewsStory.objects.order_by('-pub_date')[:4]
-        context['all_stories'] = NewsStory.objects.order_by('-pub_date')[4:]
+        context['all_stories'] = NewsStory.objects.order_by('-pub_date')[4:7]
+    
         return context
 
 """view for individual story page"""
